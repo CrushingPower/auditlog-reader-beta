91d6f0f7b36d25104ad4bb226460cb80b6bfc4fe
CrushingPower <Timmysternberg@gmail.com>
2017-01-04 14:59:05 -0500

    Update auditreader1.js

--- auditreader1.js
+++ auditreader1.js
@@ -2,4 +2,5 @@ var Check = function() {
 	console.log("TEST");
 }
 
-<select id="https://www.roblox.com/Groups/Audit.aspx?groupid=936982&pageNum=1&actionTypeId=5" onchange="Check()">
+Check();
+//<select id="https://www.roblox.com/Groups/Audit.aspx?groupid=936982&pageNum=1&actionTypeId=5" onchange="Check()">
